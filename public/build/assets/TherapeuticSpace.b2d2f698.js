e8be57ea67b8bcd4e620c2937ea5c6a460e97866
Radoslav Tomas <radoslav.tomas@gmail.com>
2022-11-15 21:47:29 +0000

    first npm build

---
+++ public/build/assets/TherapeuticSpace.b2d2f698.js
@@ -0,0 +1,1 @@
+import{o as e,c as a,w as t,a as c,u as s,H as r,b as o}from"./app.ceeb7ff0.js";import{_}from"./MainLayout.e17077a0.js";const p=o("h1",null,"Therapeutic space",-1),h={__name:"TherapeuticSpace",setup(u){return(n,i)=>(e(),a(_,null,{default:t(()=>[c(s(r),{title:"Therapeutic space"}),p]),_:1}))}};export{h as default};
